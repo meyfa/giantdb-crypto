180558ad509f19f81ec289e8104718d669e5ad1b
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-03-21 20:35:24 +0100

    Update dependencies; use JS standard style with ESLint

--- index.js
+++ index.js
@@ -1,66 +1,60 @@
-"use strict";
+'use strict'
 
-const crypto = require("crypto");
+const crypto = require('crypto')
 
 class Crypto {
-    /**
-     * Constructs a new middleware instance.
-     */
-    constructor () {
-    }
+  /**
+   * Transforms the given readable stream, putting a decryption layer in between.
+   * next is called with the modified state.
+   *
+   * @param {object} stream The base Readable Stream.
+   * @param {object} meta The item metadata.
+   * @param {object} options The user-provided options object.
+   * @param {Function} next The continuation callback.
+   * @returns {void}
+   */
+  transformReadable (stream, meta, options, next) {
+    // get initialization vector from metadata
+    const iv = Buffer.from(meta.encryption.iv, 'base64')
+    // get key from options
+    const key = options.encryption.key
 
-    /**
-     * Transforms the given readable stream, putting a decryption layer in between.
-     * next is called with the modified state.
-     *
-     * @param {stream.Readable} stream The base Readable.
-     * @param {Object} meta The item metadata.
-     * @param {Object} options The user-provided options object.
-     * @param {Function} next The continuation callback.
-     * @return {void}
-     */
-    transformReadable (stream, meta, options, next) {
-        // get initialization vector from metadata
-        const iv = Buffer.from(meta.encryption.iv, "base64");
-        // get key from options
-        const key = options.encryption.key;
+    const decipher = crypto.createDecipheriv('aes192', key, iv)
+    stream.pipe(decipher)
 
-        const decipher = crypto.createDecipheriv("aes192", key, iv);
-        stream.pipe(decipher);
+    next(null, {
+      stream: decipher
+    })
+  }
 
-        next(null, {
-            stream: decipher,
-        });
-    }
+  /**
+   * Transforms the given writable stream, putting an encryption layer in between.
+   * next is called with the modified state.
+   *
+   * @param {object} stream The base Writable Stream.
+   * @param {object} meta The item metadata.
+   * @param {object} options The user-provided options object.
+   * @param {Function} next The continuation callback.
+   * @returns {void}
+   */
+  transformWritable (stream, meta, options, next) {
+    // generate new initialization vector
+    const iv = crypto.randomBytes(16)
+    // get key from options
+    const key = options.encryption.key
 
-    /**
-     * Transforms the given writable stream, putting an encryption layer in between.
-     * next is called with the modified state.
-     *
-     * @param {stream.Writable} stream The base Writable.
-     * @param {Object} meta The item metadata.
-     * @param {Object} options The user-provided options object.
-     * @param {Function} next The continuation callback.
-     * @return {void}
-     */
-    transformWritable (stream, meta, options, next) {
-        // generate new initialization vector
-        const iv = crypto.randomBytes(16);
-        // get key from options
-        const key = options.encryption.key;
+    // store iv in metadata
+    meta.encryption = meta.encryption || {}
+    meta.encryption.iv = iv.toString('base64')
 
-        // store iv in metadata
-        meta.encryption = meta.encryption || {};
-        meta.encryption.iv = iv.toString("base64");
+    const cipher = crypto.createCipheriv('aes192', key, iv)
+    cipher.pipe(stream)
 
-        const cipher = crypto.createCipheriv("aes192", key, iv);
-        cipher.pipe(stream);
-
-        next(null, {
-            metadata: meta,
-            stream: cipher,
-        });
-    }
+    next(null, {
+      metadata: meta,
+      stream: cipher
+    })
+  }
 }
 
-module.exports = Crypto;
+module.exports = Crypto
